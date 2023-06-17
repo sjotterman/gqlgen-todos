fd78d6a045600a27db83869af362dadff8c31e1f
Samuel Otterman <sjotterman@gmail.com>
2023-06-17 15:36:37 -0400

    configure client.ts to use env var url

--- frontend/lib/client.ts
+++ frontend/lib/client.ts
@@ -1,11 +1,13 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
 
+const GRAPHQL_URL =
+  process.env.NEXT_PUBLIC_GRAPHQL_URL ?? 'http://localhost:8080/query';
 export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: 'http://localhost:8080/query',
+      uri: GRAPHQL_URL,
     }),
   });
 });
