29fdb11b0269b4c6d39ce40f25b8d6f1c5a4f6f3
Samuel Otterman <sjotterman@gmail.com>
2023-06-17 15:10:30 -0400

    use NEXT_PUBLIC for env var

--- frontend/lib/apollo-provider.tsx
+++ frontend/lib/apollo-provider.tsx
@@ -12,7 +12,8 @@ import {
   SSRMultipartLink,
 } from '@apollo/experimental-nextjs-app-support/ssr';
 
-const GRAPHQL_URL = process.env.GRAPHQL_URL ?? 'http://localhost:8080/query';
+const GRAPHQL_URL =
+  process.env.NEXT_PUBLIC_GRAPHQL_URL ?? 'http://localhost:8080/query';
 
 function makeClient() {
   console.log('makeClient');
