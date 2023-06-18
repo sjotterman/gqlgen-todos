d1eb72f853465f7843996061052a3f33b61c2755
Samuel Otterman <sjotterman@gmail.com>
2023-06-17 21:17:50 -0400

    dynamically generate apollo provider

--- frontend/lib/apollo-provider.tsx
+++ frontend/lib/apollo-provider.tsx
@@ -11,6 +11,8 @@ import {
   NextSSRInMemoryCache,
   SSRMultipartLink,
 } from '@apollo/experimental-nextjs-app-support/ssr';
+import { useEffect, useMemo, useState } from 'react';
+import { useAuth } from '@clerk/nextjs';
 
 /*
  * This client is used in the browser
@@ -19,37 +21,62 @@ import {
 const GRAPHQL_URL =
   process.env.NEXT_PUBLIC_GRAPHQL_URL ?? 'http://localhost:8080/query';
 
-function makeClient() {
-  const httpLink = new HttpLink({
-    uri: GRAPHQL_URL,
-    credentials: 'include',
-    headers: {
-      authorization: `Bearer TOKEN`,
-    },
-  });
+const getMakeClient = (token: string) => {
+  console.log('getMakeClient', { token });
+  const makeClient = () => {
+    const httpLink = new HttpLink({
+      uri: GRAPHQL_URL,
+      credentials: 'include',
+      headers: {
+        ...(token != null ? { authorization: `Bearer ${token}` } : {}),
+      },
+    });
 
-  return new ApolloClient({
-    cache: new NextSSRInMemoryCache(),
-    link:
-      typeof window === 'undefined'
-        ? ApolloLink.from([
-            new SSRMultipartLink({
-              stripDefer: true,
-            }),
-            httpLink,
-          ])
-        : httpLink,
-  });
-}
+    return new ApolloClient({
+      cache: new NextSSRInMemoryCache(),
+      link:
+        typeof window === 'undefined'
+          ? ApolloLink.from([
+              new SSRMultipartLink({
+                stripDefer: true,
+              }),
+              httpLink,
+            ])
+          : ApolloLink.from([httpLink]),
+    });
+  };
+  return makeClient;
+};
 
 function makeSuspenseCache() {
   return new SuspenseCache();
 }
 
 export const ApolloWrapper = ({ children }: React.PropsWithChildren) => {
+  const { getToken } = useAuth();
+  const [token, setToken] = useState<string | null>();
+  useEffect(() => {
+    const asyncFunc = async () => {
+      const newToken = await getToken();
+      console.log({ newToken });
+      setToken(newToken);
+    };
+    asyncFunc().catch((e) => {
+      console.error('error in async func', e);
+    });
+  });
+  const client = useMemo(() => {
+    if (token == null) {
+      return null;
+    }
+    return getMakeClient(token);
+  }, [token]);
+  if (client == null) {
+    return null;
+  }
   return (
     <ApolloNextAppProvider
-      makeClient={makeClient}
+      makeClient={client}
       makeSuspenseCache={makeSuspenseCache}
     >
       {children}
